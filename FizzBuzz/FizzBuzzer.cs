cce7f2ebc4df9b522cb04ea1ccf563de2f91ed23
Max Johnson <max.johnson@softwire.com>
2022-07-05 15:22:53 +0100

    Refactor rules into classes and let user specify which rules to apply

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,65 @@
+﻿namespace FizzBuzz
+{
+    class FizzBuzzer
+    {
+        private static List<Rule> AskForRules()
+        {
+            List<Rule> standardRules = Rule.GetStandardRules();
+            
+            Console.Write("Enter a comma-separated list of rule triggers (or none for all): ");
+            string requestedRuleString = Console.ReadLine();
+            
+            if (requestedRuleString == "")
+            {
+                return standardRules;
+            }
+
+            int[] requestedRules = requestedRuleString.Split(',').Select(int.Parse).ToArray();
+            List<Rule> ruleList = new List<Rule>();
+            foreach (Rule rule in standardRules)
+            {
+                if (requestedRules.Contains(rule.Trigger))
+                {
+                    ruleList.Add(rule);
+                }
+            }
+            return ruleList;
+        }
+
+        public void Play()
+        {
+            List<Rule> rules = AskForRules();
+            Console.Write("Enter maximum number: ");
+            int maxNum = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= maxNum; i++)
+            {
+                List<string> words = new List<string>();
+
+                foreach (Rule rule in rules)
+                {
+                    if (rule.Test(i))
+                    {
+                        rule.Apply(words);
+                    }
+                }
+                
+                // Print the number itself if no words have been printed
+                if (words.Count == 0)
+                {
+                    words.Add(i.ToString());
+                }
+                Console.WriteLine(String.Join("", words.ToArray()));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzzer = new FizzBuzzer();
+            fizzBuzzer.Play();
+        }
+    }
+}
---
+++ FizzBuzz/Rule.cs
@@ -0,0 +1,105 @@
+﻿namespace FizzBuzz;
+
+abstract class Rule
+{
+    public int Trigger { get; }
+
+    protected Rule(int trigger)
+    {
+        Trigger = trigger;
+    }
+
+    public bool Test(int number)
+    {
+        return number % Trigger == 0;
+    }
+
+    public abstract void Apply(List<string> wordList);
+
+    public static List<Rule> GetStandardRules()
+    {
+        Rule fizz = new PrintRule(3, "Fizz");
+        Rule buzz = new PrintRule(5, "Buzz");
+        Rule bang = new PrintRule(7, "Bang");
+        Rule bong = new RemoveThenPrintRule(11, "Bong");
+        Rule fezz = new InsertBeforeRule(13, "Fezz", 'B');
+        Rule reverse = new ReverseRule(17);
+
+        return new List<Rule>
+        {
+            fizz,
+            buzz,
+            bang,
+            bong,
+            fezz,
+            reverse
+        };
+    }
+}
+
+class PrintRule : Rule
+{
+    public string Word { get; }
+    
+    public PrintRule(int trigger, string word) : base(trigger)
+    {
+        Word = word;
+    }
+
+    public override void Apply(List<string> wordList)
+    {
+        wordList.Add(Word);
+    }
+}
+
+class RemoveThenPrintRule : PrintRule
+{
+    public RemoveThenPrintRule(int trigger, string word) : base(trigger, word)
+    {}
+
+    public override void Apply(List<string> wordList)
+    {
+        // Word should appear on its own
+        wordList.Clear();
+        base.Apply(wordList);
+    }
+}
+
+class InsertBeforeRule : Rule
+{
+    public string Word { get; }
+    private char LetterToFind { get; }
+    
+    public InsertBeforeRule(int trigger, string word, char letterToFind) : base(trigger)
+    {
+        Word = word;
+        LetterToFind = letterToFind;
+    }
+
+    public override void Apply(List<string> wordList)
+    {
+        // Word is inserted immediately before the first word beginning with LetterToFind,
+        // or at the end if there are none
+        int index = wordList.FindIndex(x => x[0] == LetterToFind);
+        if (index != -1)
+        {
+            wordList.Insert(index, Word);
+        }
+        else
+        {
+            wordList.Add(Word);
+        }
+    }
+}
+
+class ReverseRule : Rule
+{
+    public ReverseRule(int trigger) : base(trigger)
+    {
+    }
+
+    public override void Apply(List<string> wordList)
+    {
+        wordList.Reverse();
+    }
+}
